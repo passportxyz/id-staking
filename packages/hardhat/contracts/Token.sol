52a5502f12641ccfb5b49f066436e1a1b286f4a8
Bliss Da Sailor <meremezeukah@gmail.com>
2022-07-12 01:33:44 -0500

    token mint function

--- packages/hardhat/contracts/Token.sol
+++ packages/hardhat/contracts/Token.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract Token is ERC20 {
     constructor() payable ERC20("Gitcoin", "GTC") {}
 
-    function mint(uint256 amount) public {
+    function mintAmount(uint256 amount) public {
         _mint(msg.sender, amount);
     }
 
