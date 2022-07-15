c57321ed439e30b7a0ac116901d44a6c80059ea7
Bliss Da Sailor <meremezeukah@gmail.com>
2022-07-15 16:21:43 -0500

    stake lock init UI

--- packages/react-app/src/views/Home.jsx
+++ packages/react-app/src/views/Home.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { useContractReader } from "eth-hooks";
 import { ethers } from "ethers";
-import { Button, Divider } from "antd";
+import { Button, Divider, Form, InputNumber } from "antd";
+import moment, { duration } from "moment";
 
 // TODO : Stake, unstake, challenge
 
 const zero = ethers.BigNumber.from("0");
 
 function Home({ tx, readContracts, address, writeContracts, mainnetProvider }) {
+  const [form] = Form.useForm();
+
   const tokenBalance = ethers.utils.formatUnits(
     useContractReader(readContracts, "Token", "balanceOf", [address]) || zero,
   );
@@ -15,6 +18,17 @@ function Home({ tx, readContracts, address, writeContracts, mainnetProvider }) {
   const stakedBalance = ethers.utils.formatUnits(
     useContractReader(readContracts, "Staking", "getStakeFor", [address]) || zero,
   );
+  const [start, duration, isActiveRound] = useContractReader(readContracts, "Staking", "fetchMeta", []) || [];
+
+  console.log({ isActiveRound });
+
+  const initialize = async v => {
+    const durationInSeconds = 86400 * parseInt(v.duration);
+    // const startInSeconds = Math.floor(Date.now() / 1000) + 120;
+    const startInSeconds = Math.floor(Date.now() / 1000) + 60;
+
+    tx(writeContracts.Staking.updateMeta(startInSeconds, durationInSeconds));
+  };
 
   const mintToken = async () => {
     tx(writeContracts.Token.mintAmount(ethers.utils.parseUnits("100")));
@@ -51,6 +65,45 @@ function Home({ tx, readContracts, address, writeContracts, mainnetProvider }) {
       </div>
       <div>
         Staked Balance: {stakedBalance} {tokenSymbol}
+      </div>
+      {start && start.gt(zero) && (
+        <div>Start: {moment.unix(start.toString()).format("dddd, MMMM Do YYYY, h:mm:ss a")}</div>
+      )}
+      {duration && duration.gt(zero) && (
+        <div>End: {moment.unix(start.add(duration)).format("dddd, MMMM Do YYYY, h:mm:ss a")}</div>
+      )}
+      <div>Is active round: {isActiveRound ? "Yes" : "No"}</div>
+
+      <div style={{ marginTop: "20px" }}>
+        <Divider>Start New Staking Round</Divider>
+        <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+          <Form
+            form={form}
+            style={{ margin: "0px auto" }}
+            initialValues={{ duration: "14" }}
+            name="initialize"
+            layout="inline"
+            onFinish={initialize}
+          >
+            <Form.Item
+              name="duration"
+              rules={[
+                {
+                  required: true,
+                  message: "Duration has to be a number > 0",
+                },
+              ]}
+              label="Duration (in days)"
+            >
+              <InputNumber min={1} />
+            </Form.Item>
+            <Form.Item>
+              <Button style={{ marginRight: "10px" }} htmlType="submit">
+                Start
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
       </div>
 
       <div style={{ marginTop: "20px" }}>
@@ -94,21 +147,6 @@ function Home({ tx, readContracts, address, writeContracts, mainnetProvider }) {
           </Button>
         </div>
       </div>
-
-      {/* <div style={{ marginTop: "20px" }}>
-        <Divider>Challenge</Divider>
-        <div style={{ width: "100%", paddingLeft: "10px", paddingRight: "10px" }}>
-          <MultiAddressInput
-            ensProvider={mainnetProvider}
-            placeholder="Enter addresses for the challenge"
-            onChange={adds => setChallengeAddresses(adds)}
-          />
-
-          <Button style={{ marginTop: "10px" }} onClick={challenge} disabled={challengeAddresses.length === 0}>
-            Submit challenges
-          </Button>
-        </div>
-      </div> */}
     </div>
   );
 }
