5211863bb7f4374420e50ce79c94c40eeeb7b6f0
Lucian Hymer <lucian@lucianhymer.com>
2023-01-13 06:53:12 -0800

    Display dialog when passport isn't loading correctly

--- packages/react-app/src/views/Home.jsx
+++ packages/react-app/src/views/Home.jsx
@@ -43,7 +43,10 @@ function Home({
         const reader = new PassportReader();
 
         const newAddress = await userSigner.getAddress();
-        const newPassport = await reader.getPassport(newAddress);
+        let newPassport;
+        try {
+          newPassport = await reader.getPassport(newAddress);
+        } catch {}
         if (web3Modal?.cachedProvider && newPassport) {
           navigate("/StakeDashboard");
           setLoggedIn(true);
@@ -98,7 +101,7 @@ function Home({
       >
         <p>
           Looks like you don’t have a Passport yet! To get started on Identity Staking, create a passport on Gitcoin
-          Passport
+          Passport. If you do already have a passport, you may need to repair it in the main Gitcoin Passport app.
         </p>
       </Modal>
       <Navbar
@@ -134,8 +137,8 @@ function Home({
             <div className="text-left mt-0 text-lg text-gray-900 sm:text-xl md:mt-10 md:pr-20 md:text-xl">
               Identity Staking is a mechanism that allows you to stake your own identity or stake on somebody else's. We
               use GTC to stake, and each staking is associated with a Passport. By staking, the profile of stamps in the
-              Passport becomes more unique, which will likely result in a stronger Trust Bonus score for Grants Round
-              15.
+              Passport becomes more unique, which will likely result in a stronger Trust Bonus score for curret Grants
+              Round.
             </div>
             <div className="mt-4 w-full sm:mt-10 sm:w-1/2 md:mt-10 md:block md:w-1/2">
               <AccountHomePage
