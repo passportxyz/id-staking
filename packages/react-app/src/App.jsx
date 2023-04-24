4ed0f8590468566d3c40c4f24055036f4b7d86e1
Michael Green <michaelgreen@Michaels-MacBook-Air.local>
2023-04-24 13:36:48 -0600

    feat(app): add intercome widget

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -190,6 +190,56 @@ function App(props) {
     }
   }, [loadWeb3Modal]);
 
+  //intercom intergration app id
+  const INTERCOM_APP_ID = "xaafeyri"; //process.env.REACT_APP_INTERCOM_APP_ID || "";
+
+  //intercom widget integration
+  useEffect(() => {
+    if (window !== "undefined") {
+      window.intercomSettings = {
+        api_base: "https://api-iam.intercom.io",
+        app_id: INTERCOM_APP_ID,
+      };
+      (function () {
+        var w: any = window;
+        var ic = w.Intercom;
+        if (typeof ic === "function") {
+          ic("reattach_activator");
+          ic("update", w.intercomSettings);
+        } else {
+          var d = document;
+          var i = function () {
+            // @ts-ignore
+            i.c(arguments);
+          };
+          // @ts-ignore
+          i.q = [];
+          // @ts-ignore
+          i.c = function (args) {
+            // @ts-ignore
+            i.q.push(args);
+          };
+          w.Intercom = i;
+          var l = function () {
+            var s = d.createElement("script");
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = "https://widget.intercom.io/widget/" + INTERCOM_APP_ID;
+            var x = d.getElementsByTagName("script")[0];
+            x.parentNode?.insertBefore(s, x);
+          };
+          if (document.readyState === "complete") {
+            l();
+          } else if (w.attachEvent) {
+            w.attachEvent("onload", l);
+          } else {
+            w.addEventListener("load", l, false);
+          }
+        }
+      })();
+    }
+  }, []);
+
   return (
     <div className="App">
       <Routes>
