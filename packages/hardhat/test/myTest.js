43571845344a6666973665a70591ac84a00ecfde
Lucian Hymer <lucian@lucianhymer.com>
2023-12-28 16:00:32 -0700

    feat(contracts): added basic test for gas measurements

--- packages/hardhat/test/myTest.js
+++ packages/hardhat/test/myTest.js
@@ -1,40 +1,57 @@
 const { ethers } = require("hardhat");
-const { use, expect } = require("chai");
-const { solidity } = require("ethereum-waffle");
-
-use(solidity);
+const { expect } = require("chai");
 
 describe("My Dapp", function () {
-  let myContract;
-
-  // quick fix to let gas reporter fetch data from gas station & coinmarketcap
-  before((done) => {
-    setTimeout(done, 2000);
-  });
-
   describe("YourContract", function () {
-    it("Should deploy YourContract", async function () {
-      const YourContract = await ethers.getContractFactory("YourContract");
+    this.beforeEach(async function () {
+      [this.owner, this.user] = await ethers.getSigners();
 
-      myContract = await YourContract.deploy();
+      const Token = await ethers.getContractFactory("GTC");
+      const token = await Token.deploy(
+        this.owner.address,
+        this.owner.address,
+        Math.floor(new Date().getTime() / 1000) + 2
+      );
+
+      await token.connect(this.owner).mint(this.owner.address, 1000000);
+      await token.connect(this.owner).mint(this.user.address, 1000000);
+
+      const YourContract = await ethers.getContractFactory("IDStaking");
+
+      this.idStaking = await YourContract.deploy(token.address);
+
+      await this.idStaking.deployed();
+
+      await token.connect(this.user).approve(this.idStaking.address, 1000000);
+
+      await this.idStaking.createRound(
+        Math.floor(Date.now() / 1000) + 10,
+        100000,
+        "Test"
+      );
     });
 
     describe("setPurpose()", function () {
       it("Should be able to set a new purpose", async function () {
-        const newPurpose = "Test Purpose";
+        await this.idStaking.connect(this.user).stake(1, 100000);
 
-        await myContract.setPurpose(newPurpose);
-        expect(await myContract.purpose()).to.equal(newPurpose);
-      });
+        await this.idStaking
+          .connect(this.user)
+          .stakeUsers(1, [this.owner.address], [100000]);
 
-      it("Should emit a SetPurpose event ", async function () {
-        const [owner] = await ethers.getSigners();
+        await this.idStaking
+          .connect(this.user)
+          .stakeUsers(1, [this.owner.address], [200000]);
 
-        const newPurpose = "Another Test Purpose";
+        await this.idStaking
+          .connect(this.owner)
+          .stakeUsers(1, [this.user.address], [100000]);
 
-        expect(await myContract.setPurpose(newPurpose))
-          .to.emit(myContract, "SetPurpose")
-          .withArgs(owner.address, newPurpose);
+        await this.idStaking
+          .connect(this.owner)
+          .stakeUsers(1, [this.user.address], [200000]);
+
+        await this.idStaking.connect(this.owner).stake(1, 100000);
       });
     });
   });
